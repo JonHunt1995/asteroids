d00dbfc5bc05aaaf00978b3b43dd8bfc106b2ef1
jonathanhunt <jhun728@wgu.edu>
2025-01-12 17:19:51 -0600

    Got player to show up in game loop

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  clock = pygame.time.Clock()
+  dt = 0
   print("Starting asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,7 +19,9 @@ def main():
       if event.type == pygame.QUIT:
         return
     screen.fill("black")
+    player.draw(screen)
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
   
 
 if __name__ == "__main__":
